ef033faa4a98c89bdd7d1f36f9d08f2d9b9ac8d6
Nickop21 <ny8244@gmail.com>
2024-06-24 02:19:17 +0530

    added experience and project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Aboutme from "@/components/Aboutme";
 import Container from "@/components/Container";
 import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import HeroAbout from "@/components/HeroAbout";
 import Projects from "@/components/Projects";
 import Technologies from "@/components/Technologies"
 import Testimonial from "@/components/Testimonial";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
